3b3fbffdd969cfdf5b7879e219fdffb5f8228aab
Hayk Gevorgyan <gevorgyanh828@gmail.com>
2024-07-26 20:44:04 +0400

    last update

--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import './projects.css'
 import Project1 from '../../Components/Project1/Project1'
-import Project2 from '../../Components/Project2/Project2'
-import NewProjects from '../../Components/NewProjects/NewProjects'
 import Project3 from '../../Components/Project3/Project3'
+import NewProjects from '../../Components/NewProjects/NewProjects'
 
 const Projects = () => {
   return (
--- src/Routes/AppRouter.jsx
+++ src/Routes/AppRouter.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import Layout from '../Pages/Layout/Layout'
 import Home from '../Pages/Home/Home'
 import Contact from '../Pages/Contact/Contact'
 import Projects from '../Pages/Projects/Projects'
 
 const AppRouter = () => {
+    const ScrollToTop = () => {
+        const { pathname } = useLocation();
+      
+        useEffect(() => {
+          window.scrollTo(0, 0);
+        }, [pathname]);
+      
+        return null;
+      };
+
   return (
     <Routes>
+        {ScrollToTop()}
         <Route path='/' element={<Layout/>}>
             <Route index element={<Home/>}/>
             <Route path='/projects' element={<Projects/>}/>
